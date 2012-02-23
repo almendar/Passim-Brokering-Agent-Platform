4c6c1600e94dc17d9a64b3118bca104e940c4a95
Tomek <tomek@d630.(none)>
2012-02-23 13:54:28 +0100

    Updated MonogDB tests.

--- src/test/scala/pl/edu/pw/elka/stud/tkogut/passim/db/MongoDBConnectionTests.scala
+++ src/test/scala/pl/edu/pw/elka/stud/tkogut/passim/db/MongoDBConnectionTests.scala
@@ -1,19 +1,57 @@
 package pl.edu.pw.elka.stud.tkogut.passim.db
-import org.scalatest.FunSuite 
+import org.scalatest.FunSuite
 import com.mongodb.casbah.Imports._
-import com.mongodb._
+import org.scalatest.BeforeAndAfterAll
+import com.mongodb.DBObject
 
-class MongoDBConnectionTests extends FunSuite {
+class MongoDBConnectionTests extends FunSuite with BeforeAndAfterAll {
+
+  val NAME = "Name"
+  val SURNAME = "Surname"
+  val AGE = "AGE"
+
+  def createPerson(name: String, surname: String, age: Int) = {
+    MongoDBObject(NAME -> name,
+      SURNAME -> surname,
+      AGE -> age)
+
+  }
+
+  val people: List[MongoDBObject] = List(
+    createPerson("Jon", "Doe", 23),
+    createPerson("Anthony", "Soprano", 47),
+    createPerson("Michael", "Corleone", 29),
+    createPerson("Fredo", "Corleone", 37))
+
+  override def beforeAll() {
+    people.foreach(x => trash += x)
+  }
+
+  override def afterAll() {
+    people.foreach(x => trash -= x)
+  }
+
+  val mongo = MongoConnection()
+  val trash = mongo("db_test")("trash")
+
+  test("Searching by surname") {
+
+    val q = MongoDBObject(SURNAME -> "Corleone")
+    val result = for (x <- trash.find(q)) yield x
+    assert(result.length == 2)
+  }
+
+  test("Search by age") {
+    val q: MongoDBObject = (AGE $gt 19 $lt 30) //people in 30'es
+    val result = for (x <- trash.find(q)) yield x
+    assert(result.length == 2)
+  }
   
-  test("MongoTest") {
-    val mongo = MongoConnection()
-    val db = mongo("db_test")("trash")
-    val doc = MongoDBObject("tomek" -> "kogut",
-        "tomek" -> "johnny")
-    db+=doc
-    
-    
- 
+  test("Query with id") {
+    val person = trash.findOne(MongoDBObject(SURNAME -> "Soprano"))
+    val query_id = person.getOrElse(MongoDBObject("_id"->0))("_id")
+    val personAlt = trash.findOneByID(query_id)
+    assert(personAlt == person)
   }
 
 }
