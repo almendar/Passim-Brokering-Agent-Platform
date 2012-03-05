9a92920758103ed9bb891dab6ac58467d1bc5071
Tomasz Kogut <almendar@gmail.com>
2012-03-05 21:40:27 +0100

    Removed unnecessary files and files that were result of bad refactorizatin.

--- src/main/scala/pl/edu/pw/elka/stud/tkogut/passim/messages/EstablishDialog.scala
+++
@@ -1,5 +0,0 @@
-package pl.edu.pw.elka.stud.tkogut.passim.messages
-
-import pl.edu.pw.elka.stud.tkogut.passim.agents.Agent
-
-case class EstablishDialog(from: Agent, dialogId: String) extends Message
