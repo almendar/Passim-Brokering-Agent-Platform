e93a5ccb287eef980e0e2a1e37b900936a8a1504
Tomasz Kogut <almendar@gmail.com>
2012-03-26 16:01:24 +0200

    Created Dialect.scala

---
+++ brokering/src/main/scala/pl/edu/pw/elka/stud/tkogut/brokering/dialect/Dialect.scala
@@ -0,0 +1,29 @@
+package pl.edu.pw.elka.stud.tkogut.brokering.dialect
+
+class Dialect {
+
+  //Zbiór pojęć
+
+}
+
+class Entity {
+
+}
+
+class Attribute {
+
+}
+
+object AttributeType extends Enumeration {
+  type AttributeType = Value
+  val TIME, DATE, DATETIEM, INTEGER, FLOAT = Value
+}
+
+class BoundAttribute extends Attribute {
+
+}
+
+class UnboundAttribute extends Attribute {
+
+}
+
