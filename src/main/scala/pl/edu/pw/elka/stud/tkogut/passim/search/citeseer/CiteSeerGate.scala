719fbfee2a3e62c0f5ac7890a6ee95eb644b1411
Tomasz Kogut <almendar@gmail.com>
2012-06-07 22:25:36 +0200

    CiteSeerGate can now search for publications and people.

--- src/main/scala/pl/edu/pw/elka/stud/tkogut/passim/search/citeseer/CiteSeerGate.scala
+++ src/main/scala/pl/edu/pw/elka/stud/tkogut/passim/search/citeseer/CiteSeerGate.scala
@@ -11,10 +11,10 @@ import impl.{DefaultConnectionReuseStrategy, DefaultHttpClientConnection}
 import message.{BasicNameValuePair, BasicHttpEntityEnclosingRequest, BasicHttpRequest}
 import protocol._
 import java.io.ByteArrayInputStream
-import collection.mutable.ArrayBuffer
 import xml.{Node, XML}
 import scala.util.matching.Regex.{MatchIterator, Match}
 import scala.collection.JavaConversions._
+import scala.collection.mutable.{Map, ArrayBuffer}
 
 
 /**
@@ -24,8 +24,6 @@ import scala.collection.JavaConversions._
  * Time: 20:46
  * To change this template use File | Settings | File Templates.
  */
-
-
 
 
  class CiteSeerHtmlDownloader
@@ -52,15 +50,19 @@ import scala.collection.JavaConversions._
    context.setAttribute(ExecutionContext.HTTP_TARGET_HOST, host);
 
 
+//   def downloadSite(requestParameters : ArrayBuffer[NameValuePair]) : String  =
+//   {
+//     val encodedString = URLEncodedUtils.format(requestParameters, HTTP.DEF_CONTENT_CHARSET.displayName())
+//     return downloadSite("/search?" + encodedString)
+//   }
 
-   def downloadSite(requestParameters : ArrayBuffer[NameValuePair]) : String  =
-   {
-     //val requestEntity: HttpEntity = new UrlEncodedFormEntity(requestParameters, HTTP.DEF_CONTENT_CHARSET)
-//     val encodedString = URLEncodedUtils.format(requestParameters, HTTP.DEF_CONTENT_CHARSET)
-     val encodedString = URLEncodedUtils.format(requestParameters, HTTP.DEF_CONTENT_CHARSET.displayName())
-     return downloadSite("/search?" + encodedString)
-   }
 
+   /**
+    * Download site from CiteSeerX. This open new connection.
+    * @param requestString String that will be attached to the citeseer adres e.g.
+    *                      /viewdoc/summary;jsessionid=2C33AF829B75EDF34EAF51C3AA23B445?doi=10.1.1.155.2813
+    * @return Content of the page, raw html.
+    */
    def downloadSite(requestString : String) : String =
    {
      if (!conn.isOpen) {
@@ -83,19 +85,16 @@ import scala.collection.JavaConversions._
      return responseString
    }
 
-   def closeConnection() {
+   def closeConnection()
+   {
      if(conn.isOpen)
        conn.close();
    }
-
-
-
-
-
-
-
  }
 
+/**
+ * Hold constant values
+ */
 object CiteSeerInfoExtraction {
 
   final val LINK = "link"
@@ -108,6 +107,7 @@ object CiteSeerInfoExtraction {
   final val YEAR = "year"
   final val AUTHORS = "authors"
   final val ABSTRACT = "abstract"
+  final val PAGER = "pager"
 
   final val regexPattern = new scala.util.matching.Regex(
     """(?s)(?i)<div class=\"result\"[^>]*>(.+?)""" +
@@ -132,41 +132,40 @@ object CiteSeerInfoExtraction {
   final val abstractRegex = new scala.util.matching.Regex(
   """(?s)(?i)<div id=\"abstract\">.*?<p>(.+?)</p>.*?</div>""",ABSTRACT
   )
+
+
+  final val pagerRegex = new scala.util.matching.Regex(
+    """(?s)(?i)<div id=\"pager\">.*?<a href=\"(.*?)\">""",PAGER
+  )  //<a href=\"(.*?)\>.*?</a></div>
 }
 
 class CiteSeerInfoExtraction(htmlSite:String)
 {
 
   val extracts = ArrayBuffer[scala.collection.mutable.Map[String,String]]()
+  var pager : Option[String] = None
 
   import CiteSeerInfoExtraction._
   def extract()
   {
-
-
-
     val it = regexPattern.findAllIn(htmlSite).matchData
-    while (it.hasNext) {
+    while (it.hasNext)
+    {
       val data = scala.collection.mutable.Map[String,String]()
-
       val matcher = it.next()
       val linkText: String = matcher.group(LINK) //.replaceAll("\n","")
-      val rawTitle = CiteSeerApp.removeHtmlTags(linkRegex.findFirstMatchIn(linkText).get.group(TEXT).trim())
+      val rawTitle = CiteSeerGate.removeHtmlTags(linkRegex.findFirstMatchIn(linkText).get.group(TEXT).trim())
       data+= (TEXT -> rawTitle)
-
       val hrefPart = linkRegex.findFirstMatchIn(linkText).get.group(HREF).trim()
-
       data+= (HREF->hrefPart)
-
       val pubInfoText: String = matcher.group(PUB_INFO)
-
       data += (PUB_INFO -> pubInfoText)
       val authors = pubInfoAuthorRegex.findFirstMatchIn(pubInfoText).
         map(_.group(AUTHORS).trim.stripPrefix("by").trim).getOrElse("Unknwon")
       data+= (AUTHORS->authors)
-
       val pubyear = pubInfoYearRegex.findFirstMatchIn(pubInfoText).map(_.group(YEAR)).getOrElse("Unknown")
       data+=(YEAR->pubyear)
+      pager = pagerRegex.findFirstMatchIn(htmlSite).map(_.group(PAGER))
       extracts+=data
   }
 
@@ -175,122 +174,76 @@ class CiteSeerInfoExtraction(htmlSite:String)
 
 class CiteSeerGate
 {
-}
-
-object CiteSeerApp
-{
- def removeHtmlTags(str: String): String = {
-   return str.replaceAll("""(<(\w+)(.*?)>)|(</\w+>)""", "")
+  def getAbstracts(works: scala.collection.mutable.ArrayBuffer[scala.collection.mutable.Map[String, String]]) {
+    works.par.foreach {
+      e =>
+        val downloader = new CiteSeerHtmlDownloader;
+        val articleDetailsHtmlSite = downloader.downloadSite(e(CiteSeerInfoExtraction.HREF))
+        val abstractText =
+          CiteSeerInfoExtraction.abstractRegex.findFirstMatchIn(articleDetailsHtmlSite)
+            .map(_.group(CiteSeerInfoExtraction.ABSTRACT)).getOrElse("Unknown")
+        e += (CiteSeerInfoExtraction.ABSTRACT -> abstractText)
+        downloader.closeConnection()
+    }
   }
 
+  def searchForPublications(publicationKeyword : String, limit:Int) : List[Map[String, String]] =
+  {
+    val requestParameters = ArrayBuffer[NameValuePair]()
+    requestParameters+=(new BasicNameValuePair("q", publicationKeyword))
+    requestParameters+=(new BasicNameValuePair("submit", "Search"))
+    requestParameters+=(new BasicNameValuePair("sort", "rlv"))
+    requestParameters+=(new BasicNameValuePair("t", "doc"))
+    return processIt(requestParameters, limit)
+  }
+
+  def searchForAuthors(authorKeyword : String, limit:Int) : List[Map[String, String]] =
+  {
+    import scala.runtime.RichChar
+    var author = if(authorKeyword.exists(_.isWhitespace)) "(%s)".format(authorKeyword) else authorKeyword
+    val requestParameters = ArrayBuffer[NameValuePair]()
+    requestParameters+=(new BasicNameValuePair("q", "author:"+author))
+    requestParameters+=(new BasicNameValuePair("submit", "Search"))
+    requestParameters+=(new BasicNameValuePair("sort", "rlv"))
+    requestParameters+=(new BasicNameValuePair("t", "doc"))
+    return processIt(requestParameters, limit)
+  }
+
+  def processIt(requestParameters: ArrayBuffer[NameValuePair], limit: Int): scala.List[Map[String, String]] = {
+    val encodedString = URLEncodedUtils.format(requestParameters, HTTP.DEF_CONTENT_CHARSET.displayName())
+    val fullUrl = "/search?" + encodedString
+    println(fullUrl)
+    var nextPagina: Option[String] = Some[String](fullUrl)
+    var downloader = new CiteSeerHtmlDownloader;
+    var buffer = ArrayBuffer[scala.collection.mutable.Map[String, String]]()
+    var results = 0
+    while (nextPagina != None && results <= limit) {
+      val siteContent = downloader.downloadSite(nextPagina.get)
+      val extractor = new CiteSeerInfoExtraction(siteContent)
+      extractor.extract()
+      nextPagina = extractor.pager
+      results += extractor.extracts.length
+      //println(extractor.extracts.length)
+      buffer ++= extractor.extracts
+    }
+    val ret: List[Map[String, String]] = buffer.toList
+    return ret
+  }
+}
+
+object CiteSeerGate
+{
+  def removeHtmlTags(str: String): String = {
+    return str.replaceAll("""(<(\w+)(.*?)>)|(</\w+>)""", "")
+  }
 
   def main(args: Array[String])
   {
-    val nameValuePair = ArrayBuffer[NameValuePair]()
-    nameValuePair+=(new BasicNameValuePair("q", "Datamining"))
-    nameValuePair+=(new BasicNameValuePair("submit", "Search"))
-    nameValuePair+=(new BasicNameValuePair("sort", "rlv"))
-    nameValuePair+=(new BasicNameValuePair("t", "doc"))
-    val downloader = new CiteSeerHtmlDownloader;
-
-    val htmlDoc: String = downloader.downloadSite(nameValuePair)
-    val  extrator = new CiteSeerInfoExtraction(htmlDoc);
-    extrator.extract()
-
-
-    extrator.extracts.par.foreach { e=>
-      val downloader = new CiteSeerHtmlDownloader;
-      val articleDetailsHtmlSite = downloader.downloadSite( e( CiteSeerInfoExtraction.HREF ) )
-      val abstractText =
-        CiteSeerInfoExtraction.abstractRegex.findFirstMatchIn( articleDetailsHtmlSite )
-          .map(_.group(CiteSeerInfoExtraction.ABSTRACT)).getOrElse("Unknown")
-      e+=(CiteSeerInfoExtraction.ABSTRACT -> abstractText)
-      downloader.closeConnection()
-    }
-    println(extrator.extracts(0)(CiteSeerInfoExtraction.ABSTRACT))
-
-    /*
-
-    for(e <- extrator.extracts){
-    val articleDetailsHtmlSite = downloader.downloadSite( e( CiteSeerInfoExtraction.HREF ) )
-   // println(articleDetailsHtmlSite)
-    println(CiteSeerInfoExtraction.abstractRegex.findFirstMatchIn( articleDetailsHtmlSite ).map(_.group(CiteSeerInfoExtraction.ABSTRACT)).getOrElse("Unknown"))
-    }
-    */
-
-
-
-
-
-
-
-        //println(htmlDoc)
-        println("==============");
-        //println(htmlDoc)
-        //template """(?s)(?i)<div([^>]+)>(.+?)</div>"""
-        //processHtml(htmlDoc)
-
-        //        val iterator: Iterator[Match] = regexPattern.findAllIn(htmlDoc).matchData
-        //        println("Found results " + iterator.length)
-        //        while(iterator.hasNext)
-        //        {
-        //
-        //          val a: Match = iterator.next
-        //          val linkText: String = a.group("link")
-        //          println("&&&&&\n"+linkText+"&&&&\n\n")
-        //          val linkRegex = new scala.util.matching.Regex(
-        //            """"(?s)(?i)<a class=\"remove doc_details\" href=\"(.+?)\"[^>]*>(.+?)</a>""","href","text")
-        //          val desc = linkRegex.findFirstMatchIn(linkText)
-        //          if(desc!=None)
-        //            println(desc.get.group("text") )
-        //          else
-        //            println(None)
-        //        }
-
-        //println("First match: " + text.get.group("link"))
-        //regexPattern.
-
-
-
-
-    //makeGET(conn, host, params, httpexecutor, httpproc, context, connStrategy)
-  }
-
-  def makeGET(conn: DefaultHttpClientConnection, host: HttpHost, params: HttpParams, httpexecutor: HttpRequestExecutor, httpproc: HttpProcessor, context: BasicHttpContext, connStrategy: DefaultConnectionReuseStrategy) {
-    try {
-
-      val targets = Array[String](
-        "/search"
-      );
-
-      for (tar <- targets) {
-        if (!conn.isOpen()) {
-          val socket = new Socket(host.getHostName(), host.getPort());
-          conn.bind(socket, params);
-        }
-        val request = new BasicHttpRequest("GET", tar);
-        println(">> Request URI: " + request.getRequestLine().getUri());
-
-        request.setParams(params);
-        httpexecutor.preProcess(request, httpproc, context);
-        val response: HttpResponse = httpexecutor.execute(request, conn, context);
-        response.setParams(params);
-        httpexecutor.postProcess(response, httpproc, context);
-        //      println(response.getHeaders("Location").mkString(","))
-        System.out.println("<< Response: " + response.getStatusLine());
-        println(response.getStatusLine.getReasonPhrase)
-        System.out.println(EntityUtils.toString(response.getEntity()));
-        System.out.println("==============");
-        if (!connStrategy.keepAlive(response, context)) {
-          conn.close();
-        } else {
-          System.out.println("Connection kept alive...");
-        }
-      }
-    } finally {
-      conn.close();
+    val csg = new CiteSeerGate
+    val i = csg.searchForAuthors("Henryk Rybinski",30)
+    for (p <- i)
+    {
+      println(p.getOrElse(CiteSeerInfoExtraction.AUTHORS,"Missing authors"))
     }
   }
 }
-
-
